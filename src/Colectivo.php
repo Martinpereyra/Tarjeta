e0b159418bb1f4fc545fea3d2126ac58e3b40ee1
Martinpereyra <pereyramartinn@gmail.com>
2016-11-03 13:11:46 -0300

    Update Colectivo.php

--- src/Colectivo.php
+++ src/Colectivo.php
@@ -2,14 +2,19 @@
 
 namespace Poli\Tarjeta;
 
-class Colectivo extends Transporte {
+class Colectivo extends Transporte{
+	protected $empresa;
+	public function __construct($id,$empresa){
+		$this->id=$id;
+		$this->empresa=$empresa;
+		$this->costo=8;
+		$this->costotrans=2.64;
+		$this->tipo=1;
+	}
 
-  private $nombre;
+	public function getNombreEmpresa(){
+		return $this->empresa;
+	}
+}
 
-  private $linea;
-
-  public function __construct($nombre, $linea) {
-    $this->tipo = "colectivo";
-    $this->nombre = $nombre;
-    $this->linea = $linea;
-  }
+?>
